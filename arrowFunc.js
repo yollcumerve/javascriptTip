2c3d76fd2ba45b8a27b25385f4dd8db690cfd08b
Merve Yolcu <104626041+yollcumerve@users.noreply.github.com>
2022-11-16 12:35:13 +0300

    Update arrowFunc.js

--- arrowFunc.js
+++ arrowFunc.js
@@ -17,7 +17,7 @@ const person = {
 window.name = 'merve'
 const person = {
     name ='meryem',
-    fullName= () => {
+    fullName:() => {
         console.log(this); //5
         console.log(this.name); //6
 
