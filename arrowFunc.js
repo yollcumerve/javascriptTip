a30aa7e4dbf9e99a64ba63428f1bed5f7cc6569c
Merve Yolcu <104626041+yollcumerve@users.noreply.github.com>
2022-11-16 11:57:06 +0300

    arrowFunc1.js

---
+++ arrowFunc.js
@@ -0,0 +1,27 @@
+window.name = 'merve';
+const person = {
+    name: 'meryem',
+    full:function(){
+        console.log(this); //1
+        console.log(this.name); //2
+
+        setTimeout(function(){
+            console.log(this); //3
+            console.log(this.name); //4
+        },2000)
+    }
+}
+
+window.name = 'merve'
+const person = {
+    name ='meryem',
+    fullName= () => {
+        console.log(this); //5
+        console.log(this.name); //6
+
+        setTimeout(() => {
+            console.log(this); //7
+            console.log(this.name) //8
+        },2000)
+    }
+}
